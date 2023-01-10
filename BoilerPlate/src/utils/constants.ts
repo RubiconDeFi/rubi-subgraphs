9de9c7eae401a54869c33fc79c9f78cea98082e3
denver <denverbaumgartner@gmail.com>
2023-01-10 17:40:55 -0500

    optimism gas calc

--- BoilerPlate/src/utils/constants.ts
+++ BoilerPlate/src/utils/constants.ts
@@ -8,9 +8,11 @@ export let ZERO_BD = BigDecimal.fromString('0')
 export let ONE_BD = BigDecimal.fromString('1')
 export let BI_18 = BigInt.fromI32(18)
 export let BI_9 = BigInt.fromI32(9)
+export let BI_6 = BigInt.fromI32(6)
 
-// hardcoded stables for optimism 
+// hardcoded erc20s for optimism 
 // TODO: this should be dynamic based upon what network we are on 
+export const WETH = Address.fromString('0x4200000000000000000000000000000000000006')
 export const USDC = Address.fromString('0x7F5c764cBc14f9669B88837ca1490cCa17c31607')
 export const DAI = Address.fromString('0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1')
 export const USDT = Address.fromString('0x94b008aA00579c1307B0EF2c499aD98a8ce58e58')
