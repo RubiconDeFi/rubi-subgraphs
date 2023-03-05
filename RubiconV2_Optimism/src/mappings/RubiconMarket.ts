50017683ac81ba881acb64592960020062c6c8cb
denver <denver@rubicon.finance>
2023-03-04 17:28:16 -0700

    legacy fusion

---
+++ RubiconV2_Optimism/src/mappings/RubiconMarket.ts
@@ -0,0 +1,352 @@
+import { Bytes, ethereum } from "@graphprotocol/graph-ts"
+import { getUser } from "../utils/entities/user"
+import { updateCandles } from "../utils/entities/candles"
+import { getTransaction } from "../utils/entities/transaction"
+import { Offer, Take, Fee } from "../../generated/schema"
+import { ZERO_BI } from "../utils/constants"
+import { emitOffer, emitTake, emitCancel, emitFee, emitDelete, LogMake, LogTake, LogKill, OfferDeleted, FeeTake } from '../../generated/RubiconMarket/RubiconMarket'
+
+export function handleOffer(event: emitOffer): void {
+    
+    // decode the offer ID
+    let offerID = event.params.id.toHexString()
+    let decoded = ethereum.decode(ethereum.ValueKind.BYTES.toString(), event.params.id)
+    if (!decoded) {
+        offerID = event.params.id.toHexString()
+    } else {
+        offerID = decoded.toBigInt().toString()
+    }
+
+    // get the transaction entity
+    let transaction = getTransaction(event)
+
+    // load the maker entity (user)
+    let maker = getUser(event.params.maker)
+
+    // make the offer entity
+    let offer = new Offer(offerID)
+    offer.transaction = transaction.id
+    offer.timestamp = event.block.timestamp
+    offer.index = event.logIndex
+    offer.maker = maker.id
+    offer.pay_gem = event.params.pay_gem
+    offer.buy_gem = event.params.buy_gem
+    offer.pay_amt = event.params.pay_amt
+    offer.buy_amt = event.params.buy_amt
+    offer.paid_amt = ZERO_BI
+    offer.bought_amt = ZERO_BI
+    offer.active = true
+    offer.save()
+}
+
+export function handleTake(event: emitTake): void {
+    
+    // get the transaction entity 
+    let transaction = getTransaction(event)
+
+    // get the taker entity (user)
+    let taker = getUser(event.params.taker)
+
+    // decode the offer ID
+    let offerID = event.params.id.toHexString()
+    let decoded = ethereum.decode(ethereum.ValueKind.BYTES.toString(), event.params.id)
+    if (!decoded) {
+        offerID = event.params.id.toHexString()
+    } else {
+        offerID = decoded.toBigInt().toString()
+    }
+
+    // load the offer entity
+    let offer = Offer.load(offerID)
+    if (!offer) {
+        return
+    }
+
+    // update the offer entity
+    offer.paid_amt = offer.paid_amt.plus(event.params.take_amt)
+    offer.bought_amt = offer.bought_amt.plus(event.params.give_amt)
+
+    // if the offer is filled, mark it as inactive and update the entity 
+    if (offer.paid_amt.equals(offer.pay_amt)) {
+        offer.active = false
+        offer.removed_timestamp = event.block.timestamp
+        offer.removed_block = event.block.number
+    }
+    offer.save()
+
+    // create the take entity
+    let take = new Take(event.transaction.hash.concat(Bytes.fromByteArray(Bytes.fromBigInt(event.logIndex))))
+    take.transaction = transaction.id
+    take.timestamp = event.block.timestamp
+    take.index = event.logIndex
+    take.taker = taker.id
+    take.offer = offer.id
+    take.take_gem = offer.pay_gem
+    take.give_gem = offer.buy_gem
+    take.take_amt = event.params.take_amt
+    take.give_amt = event.params.give_amt
+    take.save()
+
+    // update the candle entities 
+    updateCandles(take)
+}
+
+export function handleCancel(event: emitCancel): void {
+    
+    // decode the offer ID
+    let offerID = event.params.id.toHexString()
+    let decoded = ethereum.decode(ethereum.ValueKind.BYTES.toString(), event.params.id)
+    if (!decoded) {
+        offerID = event.params.id.toHexString()
+    } else {
+        offerID = decoded.toBigInt().toString()
+    }
+
+    // load the offer entity
+    let offer = Offer.load(offerID)
+    if (!offer) {
+        return
+    }
+
+    // update the offer entity
+    offer.active = false
+    offer.removed_timestamp = event.block.timestamp
+    offer.removed_block = event.block.number
+    offer.save()
+}
+
+export function handleFee(event: emitFee): void {
+
+    // get the transaction entity
+    let transaction = getTransaction(event)
+
+    // get the user entity (paid the fee)
+    let user = getUser(event.params.taker)
+
+    // load the fee recipient entity
+    let recipient = getUser(event.params.feeTo)
+    
+    // decode the offer ID
+    let offerID = event.params.id.toHexString()
+    let decoded = ethereum.decode(ethereum.ValueKind.BYTES.toString(), event.params.id)
+    if (!decoded) {
+        offerID = event.params.id.toHexString()
+    } else {
+        offerID = decoded.toBigInt().toString()
+    }
+
+    // load the offer entity
+    let offer = Offer.load(offerID)
+    if (!offer) {
+        return
+    }
+
+    // create the fee entity
+    let fee = new Fee(event.transaction.hash.concat(Bytes.fromByteArray(Bytes.fromBigInt(event.logIndex))))
+    fee.transaction = transaction.id
+    fee.timestamp = event.block.timestamp
+    fee.index = event.logIndex
+    fee.user = user.id
+    fee.recipient = recipient.id
+    fee.offer = offer.id
+    fee.fee_gem = event.params.asset
+    fee.fee_amt = event.params.feeAmt
+    fee.save()
+}
+
+export function handleDelete(event: emitDelete): void {
+    
+    // decode the offer ID
+    let offerID = event.params.id.toHexString()
+    let decoded = ethereum.decode(ethereum.ValueKind.BYTES.toString(), event.params.id)
+    if (!decoded) {
+        offerID = event.params.id.toHexString()
+    } else {
+        offerID = decoded.toBigInt().toString()
+    }
+
+    // load the offer entity
+    let offer = Offer.load(offerID)
+    if (!offer) {
+        return
+    }
+
+    // update the offer entity
+    offer.active = false
+    offer.removed_timestamp = event.block.timestamp
+    offer.removed_block = event.block.number
+    offer.save()
+}
+
+// this handles the v1 contract to ensure data continuity
+export function handleLogMake(event: LogMake): void {
+    
+    // decode the offer ID
+    let offerID = event.params.id.toHexString()
+    let decoded = ethereum.decode(ethereum.ValueKind.BYTES.toString(), event.params.id)
+    if (!decoded) {
+        offerID = event.params.id.toHexString()
+    } else {
+        offerID = decoded.toBigInt().toString()
+    }
+
+    // get the transaction entity
+    let transaction = getTransaction(event)
+
+    // load the maker entity (user)
+    let maker = getUser(event.params.maker)
+
+    // make the offer entity
+    let offer = new Offer(offerID)
+    offer.transaction = transaction.id
+    offer.timestamp = event.block.timestamp
+    offer.index = event.logIndex
+    offer.maker = maker.id
+    offer.pay_gem = event.params.pay_gem
+    offer.buy_gem = event.params.buy_gem
+    offer.pay_amt = event.params.pay_amt
+    offer.buy_amt = event.params.buy_amt
+    offer.paid_amt = ZERO_BI
+    offer.bought_amt = ZERO_BI
+    offer.active = true
+    offer.save()
+}
+
+export function handleLogTake(event: LogTake): void {
+    
+    // get the transaction entity 
+    let transaction = getTransaction(event)
+
+    // get the taker entity (user)
+    let taker = getUser(event.params.taker)
+
+    // decode the offer ID
+    let offerID = event.params.id.toHexString()
+    let decoded = ethereum.decode(ethereum.ValueKind.BYTES.toString(), event.params.id)
+    if (!decoded) {
+        offerID = event.params.id.toHexString()
+    } else {
+        offerID = decoded.toBigInt().toString()
+    }
+
+    // load the offer entity
+    let offer = Offer.load(offerID)
+    if (!offer) {
+        return
+    }
+
+    // update the offer entity
+    offer.paid_amt = offer.paid_amt.plus(event.params.take_amt)
+    offer.bought_amt = offer.bought_amt.plus(event.params.give_amt)
+
+    // if the offer is filled, mark it as inactive and update the entity 
+    if (offer.paid_amt.equals(offer.pay_amt)) {
+        offer.active = false
+        offer.removed_timestamp = event.block.timestamp
+        offer.removed_block = event.block.number
+    }
+    offer.save()
+
+    // create the take entity
+    let take = new Take(event.transaction.hash.concat(Bytes.fromByteArray(Bytes.fromBigInt(event.logIndex))))
+    take.transaction = transaction.id
+    take.timestamp = event.block.timestamp
+    take.index = event.logIndex
+    take.taker = taker.id
+    take.offer = offer.id
+    take.take_gem = offer.pay_gem
+    take.give_gem = offer.buy_gem
+    take.take_amt = event.params.take_amt
+    take.give_amt = event.params.give_amt
+    take.save()
+
+    // update the candle entities 
+    updateCandles(take)
+}
+
+export function handleLogKill(event: LogKill): void {
+    
+    // decode the offer ID
+    let offerID = event.params.id.toHexString()
+    let decoded = ethereum.decode(ethereum.ValueKind.BYTES.toString(), event.params.id)
+    if (!decoded) {
+        offerID = event.params.id.toHexString()
+    } else {
+        offerID = decoded.toBigInt().toString()
+    }
+
+    // load the offer entity
+    let offer = Offer.load(offerID)
+    if (!offer) {
+        return
+    }
+
+    // update the offer entity
+    offer.active = false
+    offer.removed_timestamp = event.block.timestamp
+    offer.removed_block = event.block.number
+    offer.save()
+}
+
+export function handleFeeTake(event: FeeTake): void {
+
+    // get the transaction entity
+    let transaction = getTransaction(event)
+
+    // get the user entity (paid the fee)
+    let user = getUser(event.params.taker)
+
+    // load the fee recipient entity
+    let recipient = getUser(event.params.feeTo)
+    
+    // decode the offer ID
+    let offerID = event.params.id.toHexString()
+    let decoded = ethereum.decode(ethereum.ValueKind.BYTES.toString(), event.params.id)
+    if (!decoded) {
+        offerID = event.params.id.toHexString()
+    } else {
+        offerID = decoded.toBigInt().toString()
+    }
+
+    // load the offer entity
+    let offer = Offer.load(offerID)
+    if (!offer) {
+        return
+    }
+
+    // create the fee entity
+    let fee = new Fee(event.transaction.hash.concat(Bytes.fromByteArray(Bytes.fromBigInt(event.logIndex))))
+    fee.transaction = transaction.id
+    fee.timestamp = event.block.timestamp
+    fee.index = event.logIndex
+    fee.user = user.id
+    fee.recipient = recipient.id
+    fee.offer = offer.id
+    fee.fee_gem = event.params.asset
+    fee.fee_amt = event.params.feeAmt
+    fee.save()
+}
+
+export function handleOfferDeleted(event: OfferDeleted): void {
+    
+    // decode the offer ID
+    let offerID = event.params.id.toHexString()
+    let decoded = ethereum.decode(ethereum.ValueKind.BYTES.toString(), event.params.id)
+    if (!decoded) {
+        offerID = event.params.id.toHexString()
+    } else {
+        offerID = decoded.toBigInt().toString()
+    }
+
+    // load the offer entity
+    let offer = Offer.load(offerID)
+    if (!offer) {
+        return
+    }
+
+    // update the offer entity
+    offer.active = false
+    offer.removed_timestamp = event.block.timestamp
+    offer.removed_block = event.block.number
+    offer.save()
+}
---
+++ RubiconV2_Optimism/src/utils/entities/candles.ts
@@ -0,0 +1,176 @@
+import { BigInt } from "@graphprotocol/graph-ts";
+import { CandleOneMinute, CandleFiveMinute, CandleFifteenMinute, CandleOneHour, CandleOneDay, Take } from "../../../generated/schema"
+
+export function updateCandles(entity: Take): void {
+
+    // token0 = lower alphabetically token
+    let token0 = entity.take_gem.toHexString() < entity.give_gem.toHexString() ? entity.take_gem : entity.give_gem
+    
+    // token1 = higher alphabetically token
+    let token1 = entity.take_gem.toHexString() > entity.give_gem.toHexString() ? entity.take_gem : entity.give_gem
+    
+    // token0_amount = amount of token0 in the trade
+    let token0_amount = entity.take_gem.toHexString() < entity.give_gem.toHexString() ? entity.take_amt : entity.give_amt
+    
+    // token1_amount = amount of token1 in the trade
+    let token1_amount = entity.take_gem.toHexString() > entity.give_gem.toHexString() ? entity.take_amt : entity.give_amt
+    
+    // let ratio = token0_amount / token1_amount
+    let ratio = token0_amount.toBigDecimal().div(token1_amount.toBigDecimal())
+    
+    // get the relevant time periods for each candle 
+    let one_minute = entity.timestamp.toI32() / 60
+    let five_minute = entity.timestamp.toI32() / 300
+    let fifteen_minute = entity.timestamp.toI32() / 900
+    let one_hour = entity.timestamp.toI32() / 3600
+    let one_day = entity.timestamp.toI32() / 86400
+
+    // get the candle ids
+    let candle_one_minute_id = token0.toHexString().concat("-").concat(token1.toHexString()).concat("-").concat(BigInt.fromI32(one_minute).toString())
+    let candle_five_minute_id = token0.toHexString().concat("-").concat(token1.toHexString()).concat("-").concat(BigInt.fromI32(five_minute).toString())
+    let candle_fifteen_minute_id = token0.toHexString().concat("-").concat(token1.toHexString()).concat("-").concat(BigInt.fromI32(fifteen_minute).toString())
+    let candle_one_hour_id = token0.toHexString().concat("-").concat(token1.toHexString()).concat("-").concat(BigInt.fromI32(one_hour).toString())
+    let candle_one_day_id = token0.toHexString().concat("-").concat(token1.toHexString()).concat("-").concat(BigInt.fromI32(one_day).toString())
+
+    // load in the candles
+    let candle_one_minute = CandleOneMinute.load(candle_one_minute_id)
+    let candle_five_minute = CandleFiveMinute.load(candle_five_minute_id)
+    let candle_fifteen_minute = CandleFifteenMinute.load(candle_fifteen_minute_id)
+    let candle_one_hour = CandleOneHour.load(candle_one_hour_id)
+    let candle_one_day = CandleOneDay.load(candle_one_day_id)
+
+    if (candle_one_minute == null) {
+        candle_one_minute = new CandleOneMinute(candle_one_minute_id)
+        candle_one_minute.token0 = token0
+        candle_one_minute.token1 = token1
+        candle_one_minute.open_timestamp = entity.timestamp
+        candle_one_minute.close_timestamp = entity.timestamp
+        candle_one_minute.open = entity.id
+        candle_one_minute.high = entity.id
+        candle_one_minute.low = entity.id
+        candle_one_minute.close = entity.id
+        candle_one_minute.high_ratio = ratio
+        candle_one_minute.low_ratio = ratio
+        candle_one_minute.save()
+    } else {
+        if (ratio > candle_one_minute.high_ratio) {
+            candle_one_minute.high = entity.id
+            candle_one_minute.high_ratio = ratio
+        }
+        if (ratio < candle_one_minute.low_ratio) {
+            candle_one_minute.low = entity.id
+            candle_one_minute.low_ratio = ratio
+        }
+        candle_one_minute.close = entity.id
+        candle_one_minute.close_timestamp = entity.timestamp
+        candle_one_minute.save()
+    }
+
+    if (candle_five_minute == null) {
+        candle_five_minute = new CandleFiveMinute(candle_five_minute_id)
+        candle_five_minute.token0 = token0
+        candle_five_minute.token1 = token1
+        candle_five_minute.open_timestamp = entity.timestamp
+        candle_five_minute.close_timestamp = entity.timestamp
+        candle_five_minute.open = entity.id
+        candle_five_minute.high = entity.id
+        candle_five_minute.low = entity.id
+        candle_five_minute.close = entity.id
+        candle_five_minute.high_ratio = ratio
+        candle_five_minute.low_ratio = ratio
+        candle_five_minute.save()
+    } else {
+        if (ratio > candle_five_minute.high_ratio) {
+            candle_five_minute.high = entity.id
+            candle_five_minute.high_ratio = ratio
+        }
+        if (ratio < candle_five_minute.low_ratio) {
+            candle_five_minute.low = entity.id
+            candle_five_minute.low_ratio = ratio
+        }
+        candle_five_minute.close = entity.id
+        candle_five_minute.close_timestamp = entity.timestamp
+        candle_five_minute.save()
+    }
+
+    if (candle_fifteen_minute == null) {
+        candle_fifteen_minute = new CandleFifteenMinute(candle_fifteen_minute_id)
+        candle_fifteen_minute.token0 = token0
+        candle_fifteen_minute.token1 = token1
+        candle_fifteen_minute.open_timestamp = entity.timestamp
+        candle_fifteen_minute.close_timestamp = entity.timestamp
+        candle_fifteen_minute.open = entity.id
+        candle_fifteen_minute.high = entity.id
+        candle_fifteen_minute.low = entity.id
+        candle_fifteen_minute.close = entity.id
+        candle_fifteen_minute.high_ratio = ratio
+        candle_fifteen_minute.low_ratio = ratio
+        candle_fifteen_minute.save()
+    } else {
+        if (ratio > candle_fifteen_minute.high_ratio) {
+            candle_fifteen_minute.high = entity.id
+            candle_fifteen_minute.high_ratio = ratio
+        }
+        if (ratio < candle_fifteen_minute.low_ratio) {
+            candle_fifteen_minute.low = entity.id
+            candle_fifteen_minute.low_ratio = ratio
+        }
+        candle_fifteen_minute.close = entity.id
+        candle_fifteen_minute.close_timestamp = entity.timestamp
+        candle_fifteen_minute.save()
+    }
+
+    if (candle_one_hour == null) {
+        candle_one_hour = new CandleOneHour(candle_one_hour_id)
+        candle_one_hour.token0 = token0
+        candle_one_hour.token1 = token1
+        candle_one_hour.open_timestamp = entity.timestamp
+        candle_one_hour.close_timestamp = entity.timestamp
+        candle_one_hour.open = entity.id
+        candle_one_hour.high = entity.id
+        candle_one_hour.low = entity.id
+        candle_one_hour.close = entity.id
+        candle_one_hour.high_ratio = ratio
+        candle_one_hour.low_ratio = ratio
+        candle_one_hour.save()
+    } else {
+        if (ratio > candle_one_hour.high_ratio) {
+            candle_one_hour.high = entity.id
+            candle_one_hour.high_ratio = ratio
+        }
+        if (ratio < candle_one_hour.low_ratio) {
+            candle_one_hour.low = entity.id
+            candle_one_hour.low_ratio = ratio
+        }
+        candle_one_hour.close = entity.id
+        candle_one_hour.close_timestamp = entity.timestamp
+        candle_one_hour.save()
+    }
+
+    if (candle_one_day == null) {
+        candle_one_day = new CandleOneDay(candle_one_day_id)
+        candle_one_day.token0 = token0
+        candle_one_day.token1 = token1
+        candle_one_day.open_timestamp = entity.timestamp
+        candle_one_day.close_timestamp = entity.timestamp
+        candle_one_day.open = entity.id
+        candle_one_day.high = entity.id
+        candle_one_day.low = entity.id
+        candle_one_day.close = entity.id
+        candle_one_day.high_ratio = ratio
+        candle_one_day.low_ratio = ratio
+        candle_one_day.save()
+    } else {
+        if (ratio > candle_one_day.high_ratio) {
+            candle_one_day.high = entity.id
+            candle_one_day.high_ratio = ratio
+        }
+        if (ratio < candle_one_day.low_ratio) {
+            candle_one_day.low = entity.id
+            candle_one_day.low_ratio = ratio
+        }
+        candle_one_day.close = entity.id
+        candle_one_day.close_timestamp = entity.timestamp
+        candle_one_day.save()
+    }
+}
