0ef5cd92ae417d9d294548dfa9f24e752f24a9d7
denver <denver@rubicon.finance>
2023-03-08 13:44:52 -0500

    nyc

--- RubiconV2_Optimism/src/mappings/RubiconRouter.ts
+++ RubiconV2_Optimism/src/mappings/RubiconRouter.ts
@@ -2,9 +2,30 @@ import { Swap } from "../../generated/schema";
 import { Bytes } from "@graphprotocol/graph-ts";
 import { getUser } from "../utils/entities/user";
 import { getTransaction } from "../utils/entities/transaction";
-import { emitSwap } from "../../generated/RubiconRouter/RubiconRouter";
+import { emitSwap, LogSwap } from "../../generated/RubiconRouter/RubiconRouter";
 
 export function handleSwap(event: emitSwap): void {
+
+    // load the transaction entity
+    let transaction = getTransaction(event)
+
+    // load the swapper entity (user)
+    let swapper = getUser(event.params.recipient)
+
+    // make the swap entity
+    let swap = new Swap(event.transaction.hash.concat(Bytes.fromByteArray(Bytes.fromBigInt(event.logIndex))))
+    swap.transaction = transaction.id
+    swap.timestamp = event.block.timestamp
+    swap.index = event.logIndex
+    swap.swapper = swapper.id
+    swap.input_gem = event.params.inputERC20
+    swap.output_gem = event.params.targetERC20
+    swap.input_amt = event.params.inputAmount
+    swap.output_amt = event.params.realizedFill
+    swap.save()
+}
+
+export function handleLogSwap(event: LogSwap): void {
 
     // load the transaction entity
     let transaction = getTransaction(event)
