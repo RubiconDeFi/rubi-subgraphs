d11f187ef46bf346df7dbeb1e5cc6e7d5d65bd77
Ethereal Iska <etherealiska@gmail.com>
2023-10-21 17:45:05 -0400

    Working

--- Gladius/src/mappings/ExclusiveDutchOrderReactor.ts
+++ Gladius/src/mappings/ExclusiveDutchOrderReactor.ts
@@ -1,7 +1,7 @@
-import { BigInt, Bytes, ethereum } from "@graphprotocol/graph-ts";
+import { BigInt, Bytes, ethereum, log } from "@graphprotocol/graph-ts";
 import { updateCandles } from "../utils/entities/candles";
 import { getTransaction } from "../utils/entities/transaction";
-import { Fee } from "../../generated/schema";
+import { Fee, Take, Transaction } from "../../generated/schema";
 import { Fill } from '../../generated/ExclusiveDutchOrderReactor/ExclusiveDutchOrderReactor';
 import { getUser } from "../utils/entities/user";
 import { getPair } from "../utils/entities/pair";
@@ -10,8 +10,20 @@ import { getTake } from "../utils/entities/take";
 
 export function handleFill(event: Fill): void {
 
-    // get the transaction entity 
-    const transaction = getTransaction(event)
+    let transaction = Transaction.load(event.transaction.hash);
+
+    if (transaction) {
+        log.info("Transaction ({}) already processed", [event.transaction.hash.toHexString()])
+        return;
+    }
+
+    log.info("Processing transaction {}", [event.transaction.hash.toHexString()])
+
+    transaction = new Transaction(event.transaction.hash);
+    transaction.timestamp = event.block.timestamp;
+    transaction.block_number = event.block.number;
+    transaction.block_index = event.transaction.index;
+    transaction.save();
 
     const receipt = event.receipt
 
@@ -23,19 +35,28 @@ export function handleFill(event: Fill): void {
     let firstOutputTransferIndex: i32 = -1;
     let fills: ethereum.Log[] = [];
 
+    log.error(`Filler ${event.params.filler.toHexString()}`, [])
+    log.error(`Swapper ${event.params.swapper.toHexString()}`, [])
+
     for (let i: i32 = 0; i < receipt.logs.length; i++) {
-        if (
-            receipt.logs[i].topics[0] == Bytes.fromHexString(TRANSFER_SIGNATURE) &&
-            receipt.logs[i].topics[1] == event.params.swapper &&
-            receipt.logs[i].topics[2] == event.params.filler
-        ) inputTransfers.push(receipt.logs[i])
 
         if (
             receipt.logs[i].topics[0] == Bytes.fromHexString(TRANSFER_SIGNATURE) &&
-            receipt.logs[i].topics[1] == event.params.filler &&
-            receipt.logs[i].topics[2] == event.params.swapper &&
+            receipt.logs[i].topics[1].toHexString().slice(26) == event.params.swapper.toHexString().slice(2) &&
+            receipt.logs[i].topics[2].toHexString().slice(26) == event.params.filler.toHexString().slice(2)
+        ) {
+            inputTransfers.push(receipt.logs[i])
+            continue;
+        }
+
+        if (
+            receipt.logs[i].topics[0] == Bytes.fromHexString(TRANSFER_SIGNATURE) &&
+            receipt.logs[i].topics[1].toHexString().slice(26) == event.params.filler.toHexString().slice(2) &&
+            receipt.logs[i].topics[2].toHexString().slice(26) == event.params.swapper.toHexString().slice(2) &&
             firstOutputTransferIndex == -1
         ) {
+            log.error(`BREAKING`, [])
+
             firstOutputTransferIndex = i
             break
         }
@@ -43,26 +64,44 @@ export function handleFill(event: Fill): void {
 
     if (firstOutputTransferIndex == -1) return;
 
+    let outputTransfersTemp: ethereum.Log[] = [];
+    let feesTemp: ethereum.Log[] = [];
+
     for (let i: i32 = firstOutputTransferIndex; i < receipt.logs.length; i++) {
+
         if (
             receipt.logs[i].topics[0] == Bytes.fromHexString(TRANSFER_SIGNATURE) &&
-            receipt.logs[i].topics[1] == event.params.filler &&
-            receipt.logs[i].topics[2] == event.params.swapper
+            receipt.logs[i].topics[1].toHexString().slice(26) == event.params.filler.toHexString().slice(2) &&
+            receipt.logs[i].topics[2].toHexString().slice(26) == event.params.swapper.toHexString().slice(2)
         ) {
-            outputTransfers[fills.length] =
-                outputTransfers[fills.length] ? outputTransfers[fills.length].concat([receipt.logs[i]]) : [receipt.logs[i]]
+            outputTransfersTemp.push(receipt.logs[i])
+            continue;
         }
 
         if (
             receipt.logs[i].topics[0] == Bytes.fromHexString(TRANSFER_SIGNATURE) &&
-            receipt.logs[i].topics[1] == event.params.filler &&
-            receipt.logs[i].topics[2] !== event.params.swapper
-        ) fees[fills.length] =
-            fees[fills.length] ? fees[fills.length].concat([receipt.logs[i]]) : [receipt.logs[i]]
+            receipt.logs[i].topics[1].toHexString().slice(26) == event.params.filler.toHexString().slice(2) &&
+            receipt.logs[i].topics[2].toHexString().slice(26) !== event.params.swapper.toHexString().slice(2)
+        ) {
+            feesTemp.push(receipt.logs[i])
+            continue;
+        }
 
-        if (receipt.logs[i].topics[0] == Bytes.fromHexString(FILL_SIGNATURE))
-            fills.push(receipt.logs[i]);
+        if (receipt.logs[i].topics[0] == Bytes.fromHexString(FILL_SIGNATURE)) {
+            fills.push(receipt.logs[i])
+            outputTransfers.push(outputTransfersTemp)
+            fees.push(feesTemp)
+            outputTransfersTemp = []
+            feesTemp = []
+        }
     }
+
+    log.error(`Inputs {}\nOutputs {}\nFees {}\nFills {}`, [
+        inputTransfers.length.toString(),
+        outputTransfers.length.toString(),
+        fees.length.toString(),
+        fills.length.toString()
+    ])
 
     // get the taker and from entities (users)
     const taker = getUser(event.params.filler)
@@ -75,10 +114,10 @@ export function handleFill(event: Fill): void {
             const pair = getPair(inputTransfers[i].address, outputTransfers[i][j].address)
 
             const id = event.transaction.hash.concat(Bytes.fromByteArray(Bytes.fromBigInt(event.logIndex)));
-            const take = getTake(id);
+            const take = new Take(id);
 
             // create the take entity
-            take.transaction = transaction.id
+            take.transaction = event.transaction.hash
             take.timestamp = event.block.timestamp
             take.index = event.logIndex
             take.taker = taker.id
@@ -86,8 +125,8 @@ export function handleFill(event: Fill): void {
             take.pair = pair.id
             take.take_gem = inputTransfers[i].address
             take.give_gem = outputTransfers[i][j].address
-            take.take_amt = new BigInt(inputTransfers[i].data.toI32())
-            take.give_amt = new BigInt(outputTransfers[i][j].data.toI32())
+            take.take_amt = BigInt.fromByteArray(inputTransfers[i].data)
+            take.give_amt = BigInt.fromByteArray(outputTransfers[i][j].data)
             take.save()
 
             // update the candle entities
@@ -99,7 +138,7 @@ export function handleFill(event: Fill): void {
         for (let j: i32 = 0; j < fees[i].length; j++) {
             const feeLog = fees[i][j]
             let fee = new Fee(event.transaction.hash.concat(Bytes.fromByteArray(Bytes.fromBigInt(feeLog.logIndex))))
-            fee.amount = new BigInt(feeLog.data.toI32())
+            fee.amount = BigInt.fromByteArray(feeLog.data)
             fee.transaction = transaction.id
             fee.token = feeLog.address
             fee.recipient = feeLog.topics[2]
