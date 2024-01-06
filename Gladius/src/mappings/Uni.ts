797b068a48e3c2a9e3a518620be543a05b7b2e1c
Ethereal Iska <etherealiska@gmail.com>
2024-01-05 22:19:11 -0500

    Working uni

--- Gladius/src/mappings/Uni.ts
+++ Gladius/src/mappings/Uni.ts
@@ -1,39 +1,46 @@
 /* eslint-disable prefer-const */
 import { PoolCreated } from '../../generated/Factory/Factory'
-import { Pool, Token } from '../../generated/schema'
+import { Pool } from '../../generated/schema'
 import { Pool as PoolTemplate } from '../../generated/templates'
-import { fetchTokenSymbol, fetchTokenName } from '../utils/entities/token'
-import { log, Bytes } from '@graphprotocol/graph-ts'
+import { Address, BigInt, ByteArray, Bytes, ethereum, log } from '@graphprotocol/graph-ts'
 import { Take } from '../../generated/schema'
 import { updateCandles } from '../utils/entities/candles'
 import {
   Swap as SwapEvent
 } from '../../generated/templates/Pool/Pool'
 import { getPair } from '../utils/entities/pair'
+import { pools } from '../utils/pools.config'
+import { ZERO_BI } from '../utils/constants'
 
 export function handleSwap(event: SwapEvent): void {
-  let pool = Pool.load(event.address.toHexString())
+  let pool = Pool.load(event.address)
 
   // // hot fix for bad pricing
-  // if (pool.id == '0x9663f2ca0454accad3e094448ea6f77443880454') {
-  //   return
-  // }
+  if (event.address.toHexString() == '0x9663f2ca0454accad3e094448ea6f77443880454') {
+    return
+  }
 
-  if (!pool) return;
+  const token0 = (pool) ? pool.token0 : (pools.get(event.address.toHexString())) ? pools.get(event.address.toHexString())!.token0 : null
+  const token1 = (pool) ? pool.token1 : (pools.get(event.address.toHexString())) ? pools.get(event.address.toHexString())!.token1 : null  
 
-  let token0 = Token.load(pool.token0)
-  let token1 = Token.load(pool.token1)
-
-  if (!token0 || !token1) return;
+  if (!token0 || !token1) {
+    log.error(`Pool ${event.address.toHexString()}`, [])
+    throw "bail"
+  }
 
   // amounts - 0/1 are token deltas: can be positive or negative
   let amount0 = event.params.amount0
   let amount1 = event.params.amount1
 
+  if (amount0.equals(ZERO_BI) || amount1.equals(ZERO_BI)) return;
+
   const id = event.transaction.hash.concat(Bytes.fromByteArray(Bytes.fromBigInt(event.logIndex)));
   const take = new Take(id);
 
-  const pair = getPair(Bytes.fromUTF8(token0.id), Bytes.fromUTF8(token1.id))
+  const pair = getPair(token0, token1)
+
+  // log.error(`Swapping ${token0.toHexString()} ${token1.toHexString()}`, [])
+
 
   // create the take entity
   take.transaction = event.transaction.hash
@@ -42,8 +49,8 @@ export function handleSwap(event: SwapEvent): void {
   take.taker = event.params.recipient
   take.from_address = event.params.sender
   take.pair = pair.id
-  take.take_gem = Bytes.fromUTF8(token0.id)
-  take.give_gem = Bytes.fromUTF8(token1.id)
+  take.take_gem = token0
+  take.give_gem = token1
   take.take_amt = amount0
   take.give_amt = amount1
 
@@ -52,31 +59,25 @@ export function handleSwap(event: SwapEvent): void {
 }
 
 export function handlePoolCreated(event: PoolCreated): void {
-  let pool = new Pool(event.params.pool.toHexString()) as Pool
-  let token0 = Token.load(event.params.token0.toHexString())
-  let token1 = Token.load(event.params.token1.toHexString())
+  let pool = new Pool(event.params.pool) as Pool
 
-  // fetch info if null
-  if (token0 === null) {
-    token0 = new Token(event.params.token0.toHexString())
-    token0.symbol = fetchTokenSymbol(event.params.token0)
-    token0.name = fetchTokenName(event.params.token0)
-  }
+  // log.error(`Pool created ${event.params.pool.toHexString()} ${pool.id.toHexString()}`, [])
 
-  if (token1 === null) {
-    token1 = new Token(event.params.token1.toHexString())
-    token1.symbol = fetchTokenSymbol(event.params.token1)
-    token1.name = fetchTokenName(event.params.token1)
-  }
-
-  pool.token0 = token0.id
-  pool.token1 = token1.id
-  pool.createdAtTimestamp = event.block.timestamp
-  pool.createdAtBlockNumber = event.block.number
+  pool.token0 = event.params.token0
+  pool.token1 = event.params.token1
 
   pool.save()
   // create the tracked contract based on the template
   PoolTemplate.create(event.params.pool)
-  token0.save()
-  token1.save()
+}
+
+export function handleOnce(block: ethereum.Block): void {
+  const thisIsAnArray = pools.keys()
+  
+  let counter = 0;
+
+  for (let i = 0; i < thisIsAnArray.length; i++) {
+    counter += 1;
+    PoolTemplate.create(Address.fromString(thisIsAnArray[i]))
+  }
 }
